afdeb13413f2feddb4c1072fb3053a2f15318e2e
Johan Bönigk <jonny.boenigk@gmail.com>
2025-10-22 19:06:04 +0200

    uebung

--- cmdline_drawing/rectangles/solid_square.go
+++ cmdline_drawing/rectangles/solid_square.go
@@ -1,8 +1,15 @@
 package rectangles
 
+import "fmt"
+
 // Erwartet eine Seitenlänge `length`.
 // Zeichnet ein Quadrat mit dieser Seitenlänge auf der Konsole.
 // Das Quadrat soll komplett mit `#`-Zeichen gefüllt sein.
 func DrawSolidSquare(length int) {
-	// TODO
+	for i := 0; i < length; i++ {
+		for i := 0; i < length; i++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
 }
